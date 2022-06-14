23666071e1d8357261df1c9c9b71302f2c728197
Sky <skyrj50031@maricopa.edu>
2022-06-14 16:06:40 -0700

    Update main-view.jsx
    Imported a registration and login component to main-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 
 import axios from 'axios';
 
+import { RegistrationView } from '../registration-view/registration-view';
+
+import { LoginView } from '../login-view/login-view';
+
 import { MovieCard } from '../movie-card/movie-card';
 
 import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
     constructor() {
-        super();
-        this.state = {
-          movies: [],
-          selectedMovie: null
-        };
+      super();
+      // Initial state is set to null
+      this.state = {
+        movies: [],
+        selectedMovie: null,
+        user: null
+      };
     }
 
     componentDidMount(){
@@ -27,14 +33,43 @@ export class MainView extends React.Component {
         });
     }
 
-    setSelectedMovie(newSelectedMovie) {
+    setSelectedMovie(movie) {
         this.setState({
-            selectedMovie: newSelectedMovie
+            selectedMovie: movie
+        });
+    }
+
+    /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
+
+    onLoggedIn(user) {
+        this.setState({
+          user
+        });
+    }
+
+    onRegister(registered) {
+        this.setState({
+          registered,
         });
     }
 
     render() {
-        const { movies, selectedMovie } = this.state;
+        const { movies, selectedMovie, user } = this.state;
+
+        if (registered) {
+          return <RegistrationView onRegister={(bool) => this.onRegister(bool)} />;
+        }
+
+        /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+            //forcing a registration form for testing
+        if (!user) {
+          return (
+            <LoginView
+              onLoggedIn={(user) => this.onLoggedIn(user)}
+              onRegister={(bool) => this.onRegister(bool)}
+            />
+          );
+        }
         
         if (movies.length === 0) return <div className="main-view"/>;
 
