b48fe0e33a1037a2d2be4e2a61bf3f63985b2282
Sky <skyrj50031@maricopa.edu>
2022-06-14 16:20:58 -0700

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -10,7 +10,7 @@ import { MovieCard } from '../movie-card/movie-card';
 
 import { MovieView } from '../movie-view/movie-view';
 
-export class MainView extends React.Componen {
+export class MainView extends React.Component {
     constructor() {
       super();
       // Initial state is set to null
